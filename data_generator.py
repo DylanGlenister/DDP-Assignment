32c928075ff1f0aaddc111a33ecc9da92a81659b
Erica Thompson <turbocake17@gmail.com>
2025-05-27 17:57:44 +1000

    Feature: Archers improve, data now in reverse order

--- data_generator.py
+++ data_generator.py
@@ -22,12 +22,12 @@ rng_gen = np.random.default_rng()
 ARCHER_NUM = 100
 ENTRIES_PER_ARCHER = 100
 
-random_means = rng_gen.normal(loc=0.6, scale=0.1, size=ARCHER_NUM)
+random_means = rng_gen.normal(loc=0.8, scale=0.1, size=ARCHER_NUM)
 random_improvements = rng_gen.normal(loc=0.001, scale=0.0006, size=ARCHER_NUM)
 
 base_day_offsets = rng_gen.uniform(low=0, high=365, size=ARCHER_NUM).astype(int).tolist()
 
-base_day = datetime.fromisoformat("2023-01-01")
+base_day = datetime.fromisoformat("2025-05-27")
 
 archer_list = {
     "ArcherID":         [],
@@ -39,16 +39,20 @@ for archer in range(ARCHER_NUM):
     archer_id = archer
     mean_skill = random_means[archer]
     improvement_factor = random_improvements[archer]
-    initial_day = base_day + timedelta(days=base_day_offsets[archer])
+    initial_day = base_day - timedelta(days=base_day_offsets[archer])
     
     day_changes = rng_gen.uniform(low=1, high=7, size=ENTRIES_PER_ARCHER).astype(int).tolist()
 
     current_day = initial_day
+    current_skill = mean_skill
     for day in range(ENTRIES_PER_ARCHER):
         archer_list["ArcherID"].append(archer_id)
-        current_day = current_day + timedelta(days=day_changes[day])
+        current_day = current_day - timedelta(days=day_changes[day])
         archer_list["Date"].append(current_day)
-        archer_list["ScoreFraction"].append(rng_gen.normal(loc=mean_skill+(improvement_factor*day_changes[day]), scale=0.07, size=None))
+        current_skill = current_skill - improvement_factor*day_changes[day]
+        score_frac = rng_gen.normal(loc=current_skill, scale=0.03, size=None)
+        score_frac = score_frac if score_frac < 1 else 1
+        archer_list["ScoreFraction"].append(score_frac)
 
 
 df = pd.DataFrame(archer_list)

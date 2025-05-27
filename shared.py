9c8b3832e584f86894a8657da65696e0b8583f82
dylanglenister <117338067+DylanGlenister@users.noreply.github.com>
2025-05-27 17:36:15 +1000

    Forgot to include the shared file

---
+++ shared.py
@@ -0,0 +1,8 @@
+PATH_DATASET = './data.csv'
+
+PATH_MODELS = './models/'
+
+# Column names for the archery dataset
+COLUMN_ARCHER_ID = 'ArcherID'
+COLUMN_DATE = 'Date'
+COLUMN_SCORE = 'ScoreFraction'

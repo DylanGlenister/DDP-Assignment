f17d9ccdd57da9c7ba6f062f6a31e8efb741079e
Erica-99 <turbocake17@gmail.com>
2025-05-28 12:57:03 +1000

    Feature: Class for retrieving score history from the database

---
+++ retrieve_db_data.py
@@ -0,0 +1,70 @@
+"""
+    Used to retrieve player data from the database
+"""
+
+import pandas as pd
+import numpy as np
+
+import pymysql
+
+
+class DB_Retriever:
+    def __init__(self, host:str, database:str, username:str, password:str):
+        self.host = host
+        self.database = database
+        self.db_username = username
+        self.db_password = password
+        
+        
+    def get_scores(self, firstname:str, lastname:str, birthyear:int) -> pd.DataFrame | None:
+        connection = pymysql.connect(
+            host=self.host,
+            port=3306,
+            user=self.db_username,
+            password=self.db_password,
+            db=self.database
+        )
+        
+        cursor = connection.cursor()
+        
+        id_query_text = f"SELECT `archerID` FROM `archers` WHERE LOWER(`firstname`) LIKE LOWER(\'{firstname}%\') AND LOWER(`lastname`) LIKE LOWER(\'{lastname}%\') AND `yearOfBirth` = {birthyear};"
+
+        cursor.execute(id_query_text)
+        ids = cursor.fetchall()
+        if len(ids) == 0:
+            return None
+        
+        list_ids = [id_tuple[0] for id_tuple in ids if id_tuple is not None]
+        
+        ids_textformatted = ",".join([str(x) for x in list_ids])
+        
+        total_scores_query_text = f"SELECT `archerID`, `date`, `totalScore`, `round` FROM `allRoundScores` WHERE `archerID` in ({ids_textformatted});"
+        
+        cursor.execute(total_scores_query_text)
+        total_scores = cursor.fetchall()
+        
+        round_names = [entry[3] for entry in total_scores]
+        formatted_round_names = "\',\'".join(round_name for round_name in round_names)
+        
+        max_scores_query = f"SELECT `round_definition`.`name`, SUM(`range_definition`.`ends` * `range_definition`.`arrowsPerEnd` * 10) AS maxScore FROM `round_definition` NATURAL JOIN `range_order` NATURAL JOIN `range_definition` WHERE `round_definition`.`name` in (\'{formatted_round_names}\') GROUP BY `round_definition`.`roundDefID`;"
+        
+        cursor.execute(max_scores_query)
+        max_scores = cursor.fetchall()
+        
+        max_scores_dict = {}
+        for max_score in max_scores:
+            max_scores_dict[max_score[0]] = int(max_score[1])
+        
+        output = {
+            "ArcherID":         [],
+            "Date":             [],
+            "ScoreFraction":    []
+        }       
+        for entry in total_scores:
+            output["ArcherID"].append(entry[0])
+            output["Date"].append(entry[1])
+            output["ScoreFraction"].append(float(entry[2]/max_scores_dict[entry[3]]))
+        
+        connection.close()
+        return(pd.DataFrame(output))
+
